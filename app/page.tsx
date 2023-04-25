7485c1eb22231b6b75de02097500ddf927c7c8ac
lifeng <lifeng632@163.com>
2023-04-25 21:35:03 +0800

    feat: add Checkbox demo

--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,22 @@ import Link from "next/link"
 
 import { siteConfig } from "@/config/site"
 import { buttonVariants } from "@/components/ui/button"
-import { CardDemo } from "@/components/CardDemo"
-import { CardWithForm } from "@/components/CardWithFormDemo"
-import { AccordionDemo } from "@/components/AccordionDemo"
+
 import Box from "@/components/Box"
+import { AccordionDemo } from "@/components/AccordionDemo"
 import { AlertDemo } from "@/components/AlertDemo"
 import { AlertDialogDemo } from "@/components/AlertDialogDemo"
 import { AspectRatioDemo } from "@/components/AspectRatioDemo"
 import { AvatarDemo } from "@/components/AvatarDemo"
 import { BadgeDemo } from "@/components/BadgeDemo"
 import { ButtonDemo } from "@/components/ButtonDemo"
-import { CalendarDemo } from "@/components/CalendarDemo"
 import { CalendarDatePicker } from "@/components/CalendarDatePickerDemo"
-import { CalendarDateRangePicker } from "@/components/CalendarDateRangePickerDemo"
 import { CalendarDatePickerWithPresets } from "@/components/CalendarDatePickerWithPresetsDemo"
+import { CalendarDateRangePicker } from "@/components/CalendarDateRangePickerDemo"
+import { CalendarDemo } from "@/components/CalendarDemo"
+import { CardDemo } from "@/components/CardDemo"
+import { CardWithForm } from "@/components/CardWithFormDemo"
+import { CheckboxDemo } from "@/components/CheckboxDmeo"
 
 export default function IndexPage() {
   return (
@@ -48,43 +50,66 @@ export default function IndexPage() {
           GitHub
         </Link>
       </div>
+
+      <Box title="Accordion" desc="折叠组件">
+        <AccordionDemo />
+      </Box>
+      <Box title="Alert" desc="Displays a callout for user attention.">
+        <AlertDemo />
+      </Box>
+      <Box
+        title="AlertDialog"
+        desc="A modal dialog that interrupts the user with important content and expects a response."
+      >
+        <AlertDialogDemo />
+      </Box>
+      <Box title="Aspect Ratio" desc="Displays content within a desired ratio.">
+        <AspectRatioDemo />
+      </Box>
+      <Box
+        title="Avatar"
+        desc="An image element with a fallback for representing the user."
+      >
+        <AvatarDemo />
+      </Box>
+      <Box
+        title="Badge"
+        desc="Displays a badge or a component that looks like a badge."
+      >
+        <BadgeDemo />
+      </Box>
+
+      <Box
+        title="Button"
+        desc="Displays a button or a component that looks like a button."
+      >
+        <ButtonDemo />
+      </Box>
+
+      <Box
+        title="Calendar"
+        desc="A date field component that allows users to enter and edit date."
+      >
+        <CalendarDemo />
+        <section className="flex flex-col gap-4">
+          <CalendarDatePicker />
+          <CalendarDateRangePicker />
+          <CalendarDatePickerWithPresets />
+        </section>
+      </Box>
+
       <Box title="Card" desc="卡片组件">
         <div className="flex gap-4">
-        <CardDemo/>
-        <CardWithForm/>
+          <CardDemo />
+          <CardWithForm />
         </div>
       </Box>
 
-      <Box title="Accordion" desc="折叠组件">
-      <AccordionDemo/>
-      </Box>
-      <Box title="Alert" desc="Displays a callout for user attention.">
-        <AlertDemo/>
-      </Box>
-      <Box title="AlertDialog" desc="A modal dialog that interrupts the user with important content and expects a response.">
-        <AlertDialogDemo/>
-      </Box>
-      <Box title="Aspect Ratio" desc="Displays content within a desired ratio.">
-        <AspectRatioDemo/>
-      </Box>
-      <Box title="Avatar" desc="An image element with a fallback for representing the user.">
-        <AvatarDemo/>
-      </Box>
-      <Box title="Badge" desc="Displays a badge or a component that looks like a badge.">
-        <BadgeDemo/>
-      </Box>
-
-      <Box title="Button" desc="Displays a button or a component that looks like a button.">
-        <ButtonDemo/>
-      </Box>
-
-      <Box title="Calendar" desc="A date field component that allows users to enter and edit date.">
-        <CalendarDemo/>
-        <section className="flex flex-col gap-4">
-          <CalendarDatePicker/>
-          <CalendarDateRangePicker/>
-          <CalendarDatePickerWithPresets/>
-        </section>
+      <Box
+        title="Checkbox"
+        desc="A control that allows the user to toggle between checked and not checked."
+      >
+        <CheckboxDemo />
       </Box>
     </section>
   )
---
+++ components/CheckboxDmeo.tsx
@@ -0,0 +1,44 @@
+"use client"
+
+import { Checkbox } from "@/components/ui/checkbox"
+
+export function CheckboxDemo() {
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex items-center space-x-2">
+        <Checkbox id="terms" />
+        <label
+          htmlFor="terms"
+          className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+        >
+          Accept terms and conditions
+        </label>
+      </div>
+
+      <div className="items-top flex space-x-2">
+        <Checkbox id="terms1" />
+        <div className="grid gap-1.5 leading-none">
+          <label
+            htmlFor="terms1"
+            className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+          >
+            Accept terms and conditions
+          </label>
+          <p className="text-sm text-muted-foreground">
+            You agree to our Terms of Service and Privacy Policy.
+          </p>
+        </div>
+      </div>
+
+      <div className="flex items-center space-x-2">
+        <Checkbox id="terms2" disabled />
+        <label
+          htmlFor="terms2"
+          className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+        >
+          Accept terms and conditions
+        </label>
+      </div>
+    </div>
+  )
+}
