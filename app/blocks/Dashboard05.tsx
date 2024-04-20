ca0e5e271e29a3a62abc81558c2d6b200e147e66
cliff <clifflee1022@gmail.com>
2024-04-20 21:07:32 +0800

    fix: blocks dashboard05 aside fixed to absolute

--- app/blocks/Dashboard05.tsx
+++ app/blocks/Dashboard05.tsx
@@ -78,8 +78,8 @@ import {
 
 export function Dashboard05() {
   return (
-    <div className="flex min-h-screen w-full flex-col bg-muted/40">
-      <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
+    <div className="relative flex min-h-screen w-full flex-col bg-muted/40">
+      <aside className="absolute inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
         <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
           <Link
             href="#"
