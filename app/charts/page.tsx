1b48385be173a64cfae6359630b783504dba296b
cliff <clifflee1022@gmail.com>
2024-08-24 17:59:10 +0800

    feat: add area bar pie chart demo

--- app/charts/page.tsx
+++ app/charts/page.tsx
@@ -2,14 +2,20 @@ import React from "react"
 
 import Box from "@/components/Box"
 
+import AreaChartDemo from "./AreaChartDemo"
+import BarChartDemo from "./BarChartDemo"
 import FirstChart from "./FirstChart"
+import PieChartDemo from "./PieChartDemo"
 
 const ChartsPage = () => {
   return (
-    <div className="container flex items-center gap-6 py-6 md:p-8">
-      <Box title="First chart" className="w-1/2">
+    <div className="container grid w-1/2 items-center gap-6 py-6 md:p-8">
+      <Box title="First chart">
         <FirstChart />
       </Box>
+      <AreaChartDemo />
+      <BarChartDemo />
+      <PieChartDemo />
     </div>
   )
 }
