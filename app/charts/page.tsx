951862b4665ebae50258e35d149ffafa60b0720d
cliff <clifflee1022@gmail.com>
2024-08-25 20:14:32 +0800

    feat: add AreaChartInteractive

--- app/charts/page.tsx
+++ app/charts/page.tsx
@@ -3,19 +3,23 @@ import React from "react"
 import Box from "@/components/Box"
 
 import AreaChartDemo from "./AreaChartDemo"
+import AreaChartInteractive from "./AreaChartInteractive"
 import BarChartDemo from "./BarChartDemo"
 import FirstChart from "./FirstChart"
 import PieChartDemo from "./PieChartDemo"
 
 const ChartsPage = () => {
   return (
-    <div className="container grid w-1/2 items-center gap-6 py-6 md:p-8">
-      <Box title="First chart">
-        <FirstChart />
-      </Box>
-      <AreaChartDemo />
-      <BarChartDemo />
-      <PieChartDemo />
+    <div className="container flex flex-col items-center gap-6 py-6 md:p-8">
+      <div className="grid flex-1 scroll-mt-20 items-start gap-10 md:grid-cols-2 md:gap-6 lg:grid-cols-3 xl:gap-10">
+        <Box title="First chart">
+          <FirstChart />
+        </Box>
+        <AreaChartDemo />
+        <BarChartDemo />
+        <PieChartDemo />
+      </div>
+      <AreaChartInteractive />
     </div>
   )
 }
